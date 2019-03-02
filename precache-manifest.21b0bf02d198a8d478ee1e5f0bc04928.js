8d6872a501c9678eddd887a9b96954cb39be78ea
Victor Mireyev <victor@opennodecloud.com>
2019-03-02 12:16:23 +0300

    Updates

---
+++ precache-manifest.21b0bf02d198a8d478ee1e5f0bc04928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6c330d05a1057595e5a",
+    "url": "/static/css/main.c3320915.chunk.css"
+  },
+  {
+    "revision": "d6c330d05a1057595e5a",
+    "url": "/static/js/main.d6c330d0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "34b16adb99b4dfe20412",
+    "url": "/static/js/2.34b16adb.chunk.js"
+  },
+  {
+    "revision": "0601a1bfda2dae5d04817c837587d9c9",
+    "url": "/index.html"
+  }
+];
