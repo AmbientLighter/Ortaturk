ac85d4161a0375ea0b634eea78e655397223cf0e
Victor Mireyev <victor.mireyev@gmail.com>
2020-10-02 23:36:16 +0300

    Updates

---
+++ precache-manifest.a0d90ecb2495488ab08ad7b304a4b2d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9742a8eb3e3ee4a0e224ce84e4b839bf",
+    "url": "/ortaturk/index.html"
+  },
+  {
+    "revision": "6b4a8b097c133a4e9fb5",
+    "url": "/ortaturk/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "93e9eadb2c77a1385e9a",
+    "url": "/ortaturk/static/js/2.5ac0c583.chunk.js"
+  },
+  {
+    "revision": "6b4a8b097c133a4e9fb5",
+    "url": "/ortaturk/static/js/main.69ff9d90.chunk.js"
+  },
+  {
+    "revision": "3a630107a1854fd4a5b0",
+    "url": "/ortaturk/static/js/runtime~main.597ca6b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7948c85a37bdaf6edfa2aa67934edd58.js"
+  "/ortaturk/precache-manifest.a0d90ecb2495488ab08ad7b304a4b2d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ortaturk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
